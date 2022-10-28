a7e77c96565cadfbf25567f8ff2d7b0e7fee05c3
asaurabh74 <asaurabh@yahoo.com>
2022-10-28 14:03:25 -0400

    Fixed the build issue

--- operator-build.sh
+++ operator-build.sh
@@ -3,10 +3,13 @@
 set -e
 set -o pipefail
 
+rm -rf bin bundle config helm-charts bundle.Dockerfile Dockerfile Makefile Project watches.yaml
+
 HELM_REPO=https://rhecosystemappeng.github.io/saas-ecommerce-boutique-shop
 HELM_CHART=tenant-manager-chart
 CLUSTER_DOMAIN=apps.ocp.pebcac.org
-IMAGE_TAG_BASE="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
+IMAGE_TAG_BASE="quay.io/ecosystem-appeng/saas-tenant-operator"
+IMG="$IMAGE_TAG_BASE:v0.0.1"
 
 # Returns 0 if the OLM status is missing, 1 otherwise
 #function checkOlmStatus() {
@@ -21,7 +24,6 @@ IMAGE_TAG_BASE="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
 # Stage the operator
 operator-sdk init --verbose --plugins=helm.sdk.operatorframework.io/v1 \
   --domain=$CLUSTER_DOMAIN \
-  --project-version=3 \
   --helm-chart=$HELM_CHART \
   --helm-chart-repo=$HELM_REPO
 
@@ -33,11 +35,11 @@ operator-sdk init --verbose --plugins=helm.sdk.operatorframework.io/v1 \
 #checkOlmStatus
 
 # Make the operator image
-make docker-build docker-push IMG="$IMAGE_TAG_BASE"
+make docker-build docker-push IMG="$IMG"
 
 # Make the operator bundle by adding OLM to the operator image
-make bundle IMG="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1" IMAGE_TAG_BASE="quay.io/ecosystem-appeng/saas-tenant-operator:v0.0.1"
-make bundle-build bundle-push IMG="$IMAGE_TAG_BASE"
+make bundle IMG="$IMG" IMAGE_TAG_BASE="$IMAGE_TAG_BASE"
+make bundle-build bundle-push IMG="$IMG" IMAGE_TAG_BASE="$IMAGE_TAG_BASE"
 
 # Make a new namespace and deploy the operator
 #oc new-project boutique-operator-demo
